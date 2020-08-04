26929b956ec29cfdf93b76738d86cf1f88fef104
gammelalf <>
2020-08-04 06:00:56 +0200

    Made parse.py object oriented

--- parse.py
+++ parse.py
@@ -1,95 +1,123 @@
 #!/usr/bin/env python3
 
+from sys import argv
+
 import re
 
 
-class Namespace:
-    pass
+class Parser:
+    
+    def __init__(self):
+        """
+        Initialize all patterns
+        """
+        # Name
+        self.name = re.compile(r"<h1>(.+)</h1>")
 
+        # Description
+        self.desc = re.compile(r"<p><em>"
+                                  r"(Medium|Large|Small|Huge|Tiny|Gargantuan|Diminutive|Colossal|Fine)"  # Size
+                                  r" ([\w ]+)"  # Race
+                                  r" ?(\(.*\))?"  # Subrace (optional)
+                                  r"(, [\w \-\(\)\%]+)"  # Alignment
+                                  r"</em></p>")
 
-pattern = Namespace()
+        # Hit Points
+        self.hp = re.compile(r"<p><strong>Hit Points</strong> "
+                             r"(\d+) "  # Average
+                             r"\(([d\d +-]+)\)"  # Dice Expr
+                             r"</p>")
 
-pattern.name = re.compile(r"<h1>(.+)</h1>")
-pattern.desc = re.compile(r"<p><em>"
-                          r"(Medium|Large|Small|Huge|Tiny|Gargantuan|Diminutive|Colossal|Fine)"  # Size
-                          r" ([\w ]+)"  # Race
-                          r" ?(\(.*\))?"  # Subrace (optional)
-                          r"(, [\w \-\(\)\%]+)"  # Alignment
-                          r"</em></p>")
-pattern.hp = re.compile(r"<p><strong>Hit Points</strong> (\d+) \(([d\d +-]+)\)</p>")
-pattern.ac = re.compile(r"<p><strong>Armor Class</strong> (\d+)(.+)?</p>") # TODO make more specific
-pattern.attr = re.compile(r"<li><strong>(\w\w\w)</strong> (\d+) \(([+-]\d+)\)</li>")
-pattern.skills = re.compile(r"<p><strong>Skills</strong>(.*)</p>")
-pattern.skill = re.compile(r"(\w+) ([+-]\d+)")
-pattern.throws = re.compile(r"<p><strong>Saving Throws</strong>(.*)</p>")
-pattern.throw = re.compile(r"(\w\w\w) ([+-]\d+)")
+        # Armor Class
+        self.ac = re.compile(r"<p><strong>Armor Class</strong> "
+                             r"(\d+)"  # Numeric value
+                             r"(.+)?"  # Rest TODO
+                             r"</p>")
 
-def get_section(fpath):
-    with open(fpath) as f:
-        html = f.read()
+        # Ability Scores
+        self.attr = re.compile(r"<li><strong>"
+                               r"(\w\w\w)"  # Score name
+                               r"</strong> "
+                               r"(\d+)"  # Absolte value
+                               r"\(([+-]\d+)\)"  # Modifier
+                               r"</li>")
 
-    start = html.find(">", html.find("<section")) + 1
-    end = html.find("</section>", start)
+        # Skills
+        self.skills = re.compile(r"<p><strong>Skills</strong>(.*)</p>")  # Outer container
+        self.skill = re.compile(r"(\w+) "  # Name
+                                r"([+-]\d+)")  # Modifier
 
-    return html[start:end]
+        self.throws = re.compile(r"<p><strong>Saving Throws</strong>(.*)</p>")  # Outer container
+        self.throw = re.compile(r"(\w\w\w) "  # Ability score
+                                r"([+-]\d+)")  # Modifier
 
-def parse(html):
-    # Regex
-    name = pattern.name.search(html).group(1)
-    size, race, subrace, alignment = pattern.desc.search(html).groups()
-    hp = pattern.hp.search(html)
-    ac = pattern.ac.search(html)
-    attrs = pattern.attr.findall(html)
-    skills = pattern.skills.search(html)
-    if skills is not None:
-        skills = pattern.skill.findall(skills.group(1))
-    throws = pattern.throws.search(html)
-    if throws is not None:
-        throws = pattern.throw.findall(throws.group(1))
+    def parse(self, fpath):
+        # Read and precut
+        with open(fpath) as f:
+            html = f.read()
 
-    # Create sheet
-    sheet = {
-                "name": name,
-                "size": size,
-                "race": race,
-                "subrace": subrace,
-                "alignment": alignment,
-                "hp": hp.group(2),
-                "ac": int(ac.group(1)),
-            }
-    for attr in attrs:
-        sheet[attr[0].lower()] = int(attr[2])
+        start = html.find(">", html.find("<section")) + 1
+        end = html.find("</section>", start)
 
-    if skills is not None:
-        skill_dict = {}
-        for skill, mod in skills:
-            skill_dict[skill.lower()] = int(mod)
-        sheet["skills"] = skill_dict
+        html = html[start:end]
 
-    if throws is not None:
-        throws_dict = {}
-        for throw, mod in throws:
-            throws_dict[throw.lower()] = int(mod)
-        sheet["saving_throws"] = throws_dict
+        # Match Regex
+        name = self.name.search(html).group(1)
+        size, race, subrace, alignment = self.desc.search(html).groups()
+        hp = self.hp.search(html).group(2)
+        ac = self.ac.search(html).group(1)
+        attrs = self.attr.findall(html)
+        skills = self.skills.search(html)
+        if skills is not None:
+            skills = self.skill.findall(skills.group(1))
+        throws = self.throws.search(html)
+        if throws is not None:
+            throws = self.throw.findall(throws.group(1))
 
-    return sheet
+        # Create sheet
+        sheet = {
+                    "name": name,
+                    "size": size,
+                    "race": race,
+                    "subrace": subrace,
+                    "alignment": alignment,
+                    "hp": hp,
+                    "ac": int(ac),
+                }
+        for attr, val, mod in attrs:
+            sheet[attr.lower()] = int(mod)
+
+        if skills is not None:
+            skill_dict = {}
+            for skill, mod in skills:
+                skill_dict[skill.lower()] = int(mod)
+            sheet["skills"] = skill_dict
+
+        if throws is not None:
+            throws_dict = {}
+            for throw, mod in throws:
+                throws_dict[throw.lower()] = int(mod)
+            sheet["saving_throws"] = throws_dict
+
+        return sheet
 
 
 if __name__ == "__main__":
-    import sys, json, os
+    import json, os
 
     if not os.path.isdir("sheets"):
         os.mkdir("sheets")
 
-    if len(sys.argv) > 1:
-        files = sys.argv[1:]
+    if len(argv) > 1:
+        files = argv[1:]
     else:
         with open("list") as f:
             files = f.read().splitlines()
 
+    parser = Parser()
+
     for name in files:
-        html = get_section(f"html/{name}.html")
-        sheet = parse(html)
+        sheet = parser.parse(f"html/{name}.html")
 
         with open(f"sheets/{name.replace('-', '_')}.json", "w") as f:
             json.dump(sheet, f, indent=4)
